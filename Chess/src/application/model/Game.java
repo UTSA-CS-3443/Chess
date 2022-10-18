4e07f552b46caf7294f500ed045aa3fd6b61df73
Jack-Nelson <jack.nelson2@my.utsa.edu>
2022-10-18 10:46:04 -0500

    Added the rest of the classes according to the UML

--- Chess/src/application/model/Game.java
+++ Chess/src/application/model/Game.java
@@ -12,4 +12,24 @@ public class Game {
 	public Game() {
 		
 	}
+	
+	public void changeTurn() {
+		
+	}
+	
+	public void isCheckmate() {
+		
+	}
+	
+	public void isDraw() {
+		
+	}
+	
+	public static Game loadFEN(String FEN) {
+		
+	}
+	
+	public String toFEN() {
+		
+	}
 }
