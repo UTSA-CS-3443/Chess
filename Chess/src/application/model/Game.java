059c9643505c6b6d877083fbffee29efde2fe1a1
d-cern <duarte.cernadas@gmail.com>
2022-11-02 12:49:06 -0500

    Created Piece and its subclasses

--- Chess/src/application/model/Game.java
+++ Chess/src/application/model/Game.java
@@ -13,23 +13,41 @@ public class Game {
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void changeTurn() {
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void isCheckmate() {
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void isDraw() {
 		
 	}
-	
-	public static Game loadFEN(String FEN) {
-		
-	}
-	
-	public String toFEN() {
-		
-	}
+//	
+//	/**
+//	 * 
+//	 * @param FEN the FEN to load on board
+//	 * @return
+//	 */
+//	public static Game loadFEN(String FEN) {
+//		
+//	}
+//	
+//	/**
+//	 * 
+//	 * @return the FEN of the current game/board
+//	 */
+//	public String toFEN() {
+//		
+//	}
 }
