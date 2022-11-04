0766ea345dcb57cf6303be5fb49e60b4d2a4f06b
Roosevelt-Jr <112191259+Roosevelt-Jr@users.noreply.github.com>
2022-11-04 10:24:27 -0500

    Squashed commit of the following:
    commit 13874b5e27cebf135e51b4583fcab0a7ca14d269
    Author: Roosevelt-Jr <112191259+Roosevelt-Jr@users.noreply.github.com>
    Date:   Fri Nov 4 10:03:20 2022 -0500
    
        Enum and reset
    
        Enum Group and Pieces created
        reset method created not finished

--- Chess/src/application/model/Move.java
+++ Chess/src/application/model/Move.java
@@ -1,17 +1,5 @@
 package application.model;
 
 public class Move {
-	
-	// attributes
-	private int fromRow;
-	private int fromCol;
-	private int toRow;
-	private int toCol;
-	
-	/**
-	 * 
-	 */
-	public Move() {
-		
-	}
+
 }
