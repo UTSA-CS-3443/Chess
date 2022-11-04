13874b5e27cebf135e51b4583fcab0a7ca14d269
Roosevelt-Jr <112191259+Roosevelt-Jr@users.noreply.github.com>
2022-11-04 10:03:20 -0500

    Enum and reset
    Enum Group and Pieces created
    reset method created not finished

--- Chess/src/application/model/Game.java
+++ Chess/src/application/model/Game.java
@@ -2,4 +2,27 @@ package application.model;
 
 public class Game {
 
+
+	private String whiteName;
+	private String blackName;
+	
+	/**
+	 * 
+	 */
+	public Game() {
+		
+	}
+	
+	public void changeTurn() {
+		
+	}
+	
+	public void isCheckmate() {
+		
+	}
+	
+	public void isDraw() {
+		
+	}
+	
 }
