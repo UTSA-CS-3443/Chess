1136524114ca4afdbccc3b993038f6e42c8e9e9f
Roosevelt-Jr <112191259+Roosevelt-Jr@users.noreply.github.com>
2022-11-04 10:23:14 -0500

    Revert "Squashed commit of the following:"
    This reverts commit 31c82245b4c73cc9c889f4020354d71d2324f640.

--- Chess/src/application/model/Game.java
+++ Chess/src/application/model/Game.java
@@ -1,8 +1,8 @@
 package application.model;
 
 public class Game {
-
-
+	
+	// attributes
 	private String whiteName;
 	private String blackName;
 	
@@ -13,16 +13,41 @@ public class Game {
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void changeTurn() {
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void isCheckmate() {
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void isDraw() {
 		
 	}
-	
+//	
+//	/**
+//	 * 
+//	 * @param FEN the FEN to load on board
+//	 * @return
+//	 */
+//	public static Game loadFEN(String FEN) {
+//		
+//	}
+//	
+//	/**
+//	 * 
+//	 * @return the FEN of the current game/board
+//	 */
+//	public String toFEN() {
+//		
+//	}
 }
