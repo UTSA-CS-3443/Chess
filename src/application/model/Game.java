a9108252994802809e03f83763cf4995c3cbf159
d-cern <duarte.cernadas@gmail.com>
2022-11-03 23:08:30 -0500

    test

---
+++ src/application/model/Game.java
@@ -0,0 +1,53 @@
+package application.model;
+
+public class Game {
+	
+	// attributes
+	private String whiteName;
+	private String blackName;
+	
+	/**
+	 * 
+	 */
+	public Game() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void changeTurn() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void isCheckmate() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void isDraw() {
+		
+	}
+//	
+//	/**
+//	 * 
+//	 * @param FEN the FEN to load on board
+//	 * @return
+//	 */
+//	public static Game loadFEN(String FEN) {
+//		
+//	}
+//	
+//	/**
+//	 * 
+//	 * @return the FEN of the current game/board
+//	 */
+//	public String toFEN() {
+//		
+//	}
+}
