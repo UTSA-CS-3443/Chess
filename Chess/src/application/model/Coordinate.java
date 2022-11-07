4fe4d3c2f11cb5d5e94e8ae9290f591a0e4f0bcf
d-cern <duarte.cernadas@gmail.com>
2022-11-07 00:02:59 -0600

    added functionality to toAlgebraicNotation() method

--- Chess/src/application/model/Coordinate.java
+++ Chess/src/application/model/Coordinate.java
@@ -13,6 +13,18 @@ public class Coordinate {
 	public Coordinate offset(int dRow, int dCol) {
 		return new Coordinate(this.getRow() + dRow, this.getCol() + dCol);
 	}
+	
+	/**
+	 * Returns Coordinate in algebraic notation
+	 * i.e., (3,4) == c4
+	 * 
+	 * 'a' == 97, ... , 'h' == 104
+	 * 
+	 * @return Algebraic notation
+	 */
+	public String toAlgebraicNotation() {
+		return (String) ( (char) (this.col + 97) + ((Integer) this.row).toString());
+	}
 
 	public int getRow() {
 		return row;
