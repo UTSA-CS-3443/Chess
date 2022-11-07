40eafa86be0461ed0efa210c4fc3e13303d249e6
d-cern <duarte.cernadas@gmail.com>
2022-11-07 00:08:00 -0600

    changed Move class attributes to Coordinate objects

--- Chess/src/application/model/Move.java
+++ Chess/src/application/model/Move.java
@@ -1,25 +1,56 @@
 package application.model;
 
+/**
+ * The Move class represents a move by a piece
+ * and contains from location and to location
+ * 
+ * @author Duarte Cern qag985
+ * @date 11/06/2022
+ */
 public class Move {
 	
-	// attributes
-	private int fromRow;
-	private int fromCol;
-	private int toRow;
-	private int toCol;
-	
-	/**
-	 * 
-	 */
+	private Coordinate from;
+	private Coordinate to;
+
 	public Move(int fromRow, int fromCol, int toRow, int toCol) {
-		
+		this(new Coordinate(fromRow, fromCol), new Coordinate(toRow, toCol));
 	}
 	
 	public Move(Coordinate from, Coordinate to) {
-		
+		this.from = from;
+		this.to = to;
 	}
 	
 	public boolean isLegal() {
-		
+		return false;
 	}
+
+	/**
+	 * @return the from
+	 */
+	public Coordinate getFrom() {
+		return from;
+	}
+
+	/**
+	 * @param from the from to set
+	 */
+	public void setFrom(Coordinate from) {
+		this.from = from;
+	}
+
+	/**
+	 * @return the to
+	 */
+	public Coordinate getTo() {
+		return to;
+	}
+
+	/**
+	 * @param to the to to set
+	 */
+	public void setTo(Coordinate to) {
+		this.to = to;
+	}
+	
 }
