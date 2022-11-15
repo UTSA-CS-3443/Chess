6eb4e1ca43a69d3531bbfef6c1cd02cfee8bf66a
Jack-Nelson <jack.nelson2@my.utsa.edu>
2022-11-15 17:01:47 -0600

    Squashed commit of the following:
    commit 5d0f7623f2c2b58fa1f1f5269fc5128003c3fcf5
    Merge: 37216c1 461ad9d
    Author: Jack-Nelson <jack.nelson2@my.utsa.edu>
    Date:   Tue Nov 15 16:39:57 2022 -0600
    
        Merge branch 'Jack-Nelson' into testing-merge
    
    commit 461ad9d2ac4cde05d08c79f10c80987173c50668
    Author: Jack-Nelson <jack.nelson2@my.utsa.edu>
    Date:   Fri Nov 4 11:44:43 2022 -0500
    
        Update ChessBoard.fxml
    
    commit 7d9d3fa8d5d832f9dd93be81c21249df85af90a5
    Author: Jack-Nelson <jack.nelson2@my.utsa.edu>
    Date:   Fri Nov 4 11:13:32 2022 -0500
    
        update chessboard gui
    
    commit d1eeac81c085a6c4009736772a7c10b1cc2e9703
    Author: Jack-Nelson <jack.nelson2@my.utsa.edu>
    Date:   Fri Nov 4 11:11:16 2022 -0500
    
        Update ChessBoard.fxml
    
    commit 66184122a4ca68aa44a0d3a7c72fa97c396f8d4a
    Author: Jack-Nelson <jack.nelson2@my.utsa.edu>
    Date:   Fri Nov 4 10:41:48 2022 -0500
    
        adjusting start page for gui
    
    commit 37216c11f6eb9019f3ce9c4ca6048058e9b5f98a
    Merge: 31c8224 2dd090d
    Author: Jack-Nelson <jack.nelson2@my.utsa.edu>
    Date:   Fri Nov 4 10:34:16 2022 -0500
    
        Merge branch 'Jack-Nelson' into testing-merge
    
    commit 2dd090dbae8ce0f45f36e5ab3570c8b4683b8467
    Author: Jack-Nelson <jack.nelson2@my.utsa.edu>
    Date:   Thu Nov 3 21:44:45 2022 -0500
    
        first attemp at the gui

---
+++ Chess/src/application/controller/ChessBoardController.java
@@ -0,0 +1,21 @@
+package application.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+
+public class ChessBoardController {
+
+    @FXML
+    private Label player1Label;
+
+    @FXML
+    private GridPane ChessBoard;
+
+    @FXML
+    private GridPane stageBase;
+
+    @FXML
+    private Label player2Label;
+
+}
