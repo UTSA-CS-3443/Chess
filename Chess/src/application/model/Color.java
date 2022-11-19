ac0062ebe25b7f0baabbc7a019c978973e712cf0
Blake Herrera <blakeherrera1@gmail.com>
2022-11-19 14:07:07 -0600

    Added invert method.

--- Chess/src/application/model/Color.java
+++ Chess/src/application/model/Color.java
@@ -6,5 +6,16 @@ package application.model;
  * @author Blake Herrera xng021
  */
 public enum Color {
+	
 	WHITE, BLACK;
+	
+	/**
+	 * The invert function swaps the color, similar
+	 * to a boolean not function.
+	 * @return BLACK, if the color was WHITE.
+	 * WHITE, if the color was BLACK.
+	 */
+	public Color invert() {
+		return this == WHITE ? BLACK : WHITE;
+	}
 }
