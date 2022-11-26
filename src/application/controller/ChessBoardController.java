6dab91220aeb52685434a913b1e4d5929bc62451
d-cern <83672965+d-cern@users.noreply.github.com>
2022-11-26 03:41:32 -0600

    Moved MyButton into ChessBoardController; +functionality for showFenBtn

--- src/application/controller/ChessBoardController.java
+++ src/application/controller/ChessBoardController.java
@@ -3,7 +3,6 @@ package application.controller;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -15,7 +14,6 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.Effect;
 import javafx.scene.effect.Shadow;
 import javafx.scene.image.Image;
@@ -23,9 +21,9 @@ import javafx.scene.image.ImageView;
 import application.Main;
 import application.model.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Popup;
-import javafx.scene.paint.Color;
 
 
 /**
@@ -36,6 +34,7 @@ import javafx.scene.paint.Color;
 public class ChessBoardController implements EventHandler<ActionEvent>, Initializable{
 	Game game = GameController.game;
 	public static MyButton[][] buttons = new MyButton[8][8];
+	private MyButton lastSquareClicked;
 	static Popup popup = new Popup();
 	
 	@FXML
@@ -48,6 +47,10 @@ public class ChessBoardController implements EventHandler<ActionEvent>, Initiali
 	private Label player2Label;
 	@FXML
 	GridPane ChessBoard;
+	@FXML
+	private Button showFenBtn;
+	@FXML
+	private TextField fenLabel;
 
 
 	/**
@@ -56,8 +59,16 @@ public class ChessBoardController implements EventHandler<ActionEvent>, Initiali
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
-		player2Label.setText(game.getWhiteName());
-		player1Label.setText(game.getBlackName());
+		player2Label.setText(game.getBlackName());
+		player1Label.setText(game.getWhiteName());
+		if(game.getTurn() == PieceColor.WHITE) {
+			player1Label.setTextFill(Color.YELLOWGREEN);
+			player2Label.setTextFill(Color.BLACK);
+		} else {
+			player2Label.setTextFill(Color.YELLOWGREEN);
+			player1Label.setTextFill(Color.BLACK);
+		}
+		fenLabel.setVisible(false);
 
 		for (int row = 0; row < 8; row++) {
 			for (int col = 0; col < 8; col ++) {
@@ -68,7 +79,7 @@ public class ChessBoardController implements EventHandler<ActionEvent>, Initiali
 					public void handle(ActionEvent e) {
 						tile.handle(e);
 					}
-				});//CHECK HERE FOR ERROR....Maybe should go into the handle method?
+				}); //CHECK HERE FOR ERROR....Maybe should go into the handle method?
 				String color ;
 				if ((row + col) % 2 == 0) {
 					color = "white";
@@ -89,10 +100,22 @@ public class ChessBoardController implements EventHandler<ActionEvent>, Initiali
 	}
 
 	/**
-	 *handles the event that the home button is pressed
+	 * handles the event that the home or fen button is pressed
 	 */
 	@Override
 	public void handle(ActionEvent event) {
+		Button button = (Button) event.getSource();
+		
+		if(button.getId().equals("showFenBtn")) {
+			if(!fenLabel.isVisible()) {
+				fenLabel.setVisible(true);
+				fenLabel.setText(game.getFEN());
+			} else {
+				fenLabel.setVisible(false);
+			}
+			
+			return;
+		}
 		popup.hide();
 		try {
 			FXMLLoader loader = new FXMLLoader();
@@ -105,171 +128,186 @@ public class ChessBoardController implements EventHandler<ActionEvent>, Initiali
 			e.printStackTrace();
 		}
 	}
-}
-
-
-/**
- * creates and handles the MyButton objects and their methods
- * @author Jack Nelson tge389
- *
- */
-class MyButton extends Button implements EventHandler<ActionEvent>{
-
-	Game game = GameController.game;
-	private final int r;
-	private final int c;
-
-	private boolean isHighlighted;
-	private static MyButton lastSquareClicked;
-	private boolean hasPiece;
-
-	public MyButton(int r,int c) {
-		this.r = r;
-		this.c = c;
-	}
-
+	
 	/**
-	 * updates the image to its new location and removes the graphic from buttons where the piece has moved away from
+	 * creates and handles the MyButton objects and their methods
+	 * @author Jack Nelson tge389
+	 *
 	 */
-	public void updateImage() {
-		Piece piece = game.getPieceAt(this.getRow(),this.getCol());
-		if(piece != null) {
-			this.hasPiece = true;
-			ImageView imageView = getImageFromPiece(piece);
-			this.setGraphic(imageView);
+	class MyButton extends Button implements EventHandler<ActionEvent>{
+
+		Game game = GameController.game;
+		private final int r;
+		private final int c;
+
+		private boolean isHighlighted;
+		private boolean hasPiece;
+
+		public MyButton(int r,int c) {
+			this.r = r;
+			this.c = c;
 		}
-		else
-			this.setGraphic(null);
-	}
 
-	/**
-	 * returns the proper ImageView depending on the piece selected
-	 * @param piece the piece on the board(Piece)
-	 * @return ImageView(image) the image of the selected piece
-	 */
-	public ImageView getImageFromPiece(Piece piece) {
-		if(piece != null) {
-			try {
-				return new ImageView(new Image(new FileInputStream(piece.getImageURL())));
-			} catch(FileNotFoundException e) {
-				e.printStackTrace();
+		/**
+		 * updates the image to its new location and removes the graphic from buttons where the piece has moved away from
+		 */
+		public void updateImage() {
+			Piece piece = game.getPieceAt(this.getRow(),this.getCol());
+			if(piece != null) {
+				this.hasPiece = true;
+				ImageView imageView = getImageFromPiece(piece);
+				this.setGraphic(imageView);
 			}
+			else this.setGraphic(null);
 		}
-		return null;
-	}
 
-	/**
-	 * this method handles the actions performed on the chess board
-	 * @param event the event that a piece is selected(ActionEvent)
-	 */
-	public void handle(ActionEvent event) {
-		int row = this.getRow();
-		int col = this.getCol();
-		System.out.println(row + "" + col + " is highlighted: " + this.isHighlighted);
-		if(this.isHighLighted()) {
-			Move move = new Move(lastSquareClicked.getRow(),
-									lastSquareClicked.getCol(),
-									this.getRow(),
-									this.getCol());
-			game.pushMove(move);
-			System.out.println(game);
-			lastSquareClicked = null;
+		/**
+		 * returns the proper ImageView depending on the piece selected
+		 * @param piece the piece on the board(Piece)
+		 * @return ImageView(image) the image of the selected piece
+		 */
+		public ImageView getImageFromPiece(Piece piece) {
+			if(piece != null) {
+				try {
+					return new ImageView(new Image(new FileInputStream(piece.getImageURL())));
+				} catch(FileNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+			return null;
+		}
 
-			//update graphics
-			for(int r = 0; r < 8; r++) {
-				for(int c = 0; c < 8; c++) {
-					ChessBoardController.buttons[r][c].updateImage();
-					if(ChessBoardController.buttons[r][c].getGraphic() != null) {
+		/**
+		 * this method handles the actions performed on the chess board
+		 * @param event the event that a piece is selected(ActionEvent)
+		 */
+		public void handle(ActionEvent event) {
+			int row = this.getRow();
+			int col = this.getCol();
+			System.out.println(row + "" + col + " is highlighted: " + this.isHighlighted);
+			if(this.isHighLighted()) {
+				Move move = new Move(lastSquareClicked.getRow(),
+										lastSquareClicked.getCol(),
+										this.getRow(),
+										this.getCol());
+				
+				// update player labels
+				if(game.getTurn() == PieceColor.WHITE) {
+					player2Label.setTextFill(Color.YELLOWGREEN);
+					player1Label.setTextFill(Color.BLACK);
+				} else {
+					player1Label.setTextFill(Color.YELLOWGREEN);
+					player2Label.setTextFill(Color.BLACK);
+				}
+
+				game.pushMove(move);
+				System.out.println(game);
+				lastSquareClicked = null;
+				
+				
+				//update graphics
+				for(int r = 0; r < 8; r++) {
+					for(int c = 0; c < 8; c++) {
+						ChessBoardController.buttons[r][c].updateImage();
+						if(ChessBoardController.buttons[r][c].getGraphic() != null) {
+						}
 					}
 				}
-			}
 
-			if(game.isCheckmate()) {
-				Label label = new Label("CheckMate");
-				label.setPrefWidth(400);
-				label.setPrefHeight(200);
-				label.setAlignment(Pos.BOTTOM_CENTER);
-				label.setFont(new Font(50));
-				label.setTextFill(Color.valueOf("Black"));
-				ChessBoardController.popup.getContent().add(label);
-				ChessBoardController.popup.show(Main.stage);
-			}
+				if(game.isCheckmate()) {
+					Label label = new Label("CheckMate");
+					label.setPrefWidth(400);
+					label.setPrefHeight(200);
+					label.setAlignment(Pos.BOTTOM_CENTER);
+					label.setFont(new Font(50));
+					label.setStyle("-fx-text-fill: black");
+					ChessBoardController.popup.getContent().add(label);
+					ChessBoardController.popup.show(Main.stage);
+				}
 
-			else if(game.isDraw()) {
-				Label label = new Label("Draw");
-				label.setPrefWidth(400);
-				label.setPrefHeight(200);
-				label.setAlignment(Pos.BOTTOM_CENTER);
-				label.setFont(new Font(50));
-				label.setTextFill(Color.valueOf("Black"));
-				ChessBoardController.popup.getContent().add(label);
-				ChessBoardController.popup.show(Main.stage);
-			}
-			resetHighLightedSquares();
-		} else {
-			resetHighLightedSquares();
-			System.out.println();
-			System.out.println(this);
-			if(this.hasPiece) {
-				Piece piece = game.getPieceAt(row, col);
-				Coordinate cord = new Coordinate(row,col);
-				List<Move> legalMoves = piece.getLegalMoves(game, cord);
-				for(Move move: legalMoves){
-					int r = move.getToRow();
-					int c = move.getToCol();
+				else if(game.isDraw()) {
+					Label label = new Label("Draw");
+					label.setPrefWidth(400);
+					label.setPrefHeight(200);
+					label.setAlignment(Pos.BOTTOM_CENTER);
+					label.setFont(new Font(50));
+					label.setStyle("-fx-text-fill: black");
+					ChessBoardController.popup.getContent().add(label);
+					ChessBoardController.popup.show(Main.stage);
+				}
+				resetHighLightedSquares();
+			} else {
+				resetHighLightedSquares();
+				System.out.println();
+				System.out.println(this);
+				if(this.hasPiece) {
+					Piece piece = game.getPieceAt(row, col);
+					Coordinate cord = new Coordinate(row,col);
+					List<Move> legalMoves = piece.getLegalMoves(game, cord);
+					for(Move move: legalMoves){
+						int r = move.getToRow();
+						int c = move.getToCol();
 
-					highLightSquare(r,c);
+						highLightSquare(r,c);
+					}
+				}
+				lastSquareClicked = this;
+			}
+			
+			// update FEN label if showing FEN
+			if(fenLabel.isVisible())
+				fenLabel.setText(game.getFEN());
+		}
+
+		public int getRow() {
+			return this.r;
+		}
+
+		public int getCol() {
+			return this.c;
+		}
+
+		public boolean isHighLighted() {
+			//System.out.println("Checking highlighted status: " + this.isHighlighted);
+			return this.isHighlighted;
+		}
+
+		/**
+		 * iterates through entire board resetting all values effect values on buttons to default value(null)
+		 * @return boolean the new isHighlighted value for each button
+		 */
+		public boolean resetHighLightedSquares() {
+			//System.out.println("highlight is reset");
+			for(int r = 0; r< 8; r++) {
+				for(int c = 0; c < 8; c++) {
+					ChessBoardController.buttons[r][c].setEffect(null);
+					ChessBoardController.buttons[r][c].isHighlighted = false;
 				}
 			}
-			lastSquareClicked = this;
+			
+			return this.isHighlighted = false;
 		}
-	}
 
-	public int getRow() {
-		return this.r;
-	}
-
-	public int getCol() {
-		return this.c;
-	}
-
-	public boolean isHighLighted() {
-		//System.out.println("Checking highlighted status: " + this.isHighlighted);
-		return this.isHighlighted;
-	}
-
-	/**
-	 * iterates through entire board resetting all values effect values on buttons to default value(null)
-	 * @return boolean the new isHighlighted value for each button
-	 */
-	public boolean resetHighLightedSquares() {
-		//System.out.println("highlight is reset");
-		for(int r = 0; r< 8; r++) {
-			for(int c = 0; c < 8; c++) {
-				ChessBoardController.buttons[r][c].setEffect(null);
-				ChessBoardController.buttons[r][c].isHighlighted = false;
-			}
+		/**
+		 * highlights spaces of legal moves based on piece selected
+		 * @param r the row of the square to be highlighted (int)
+		 * @param c the column of the square to be highlighted(int)
+		 * @return boolean  the isHighlighted boolean value now set to true
+		 */
+		public boolean highLightSquare(int r, int c) {
+			Shadow shadow = new Shadow();
+			shadow.setColor(Color.YELLOW);
+			ChessBoardController.buttons[r][c].setEffect(shadow);
+			return ChessBoardController.buttons[r][c].isHighlighted = true;
 		}
-		
-		return this.isHighlighted = false;
-	}
 
-	/**
-	 * highlights spaces of legal moves based on piece selected
-	 * @param r the row of the square to be highlighted (int)
-	 * @param c the column of the square to be highlighted(int)
-	 * @return boolean  the isHighlighted boolean value now set to true
-	 */
-	public boolean highLightSquare(int r, int c) {
-		Shadow shadow = new Shadow();
-		shadow.setColor(Color.YELLOW);
-		ChessBoardController.buttons[r][c].setEffect(shadow);
-		return ChessBoardController.buttons[r][c].isHighlighted = true;
-	}
-
-	@Override
-	public String toString() {
-		return "MyButton [game=" + game + ", r=" + r + ", c=" + c + ", isHighlighted=" + isHighlighted + ", hasPiece="
-				+ hasPiece + "]";
+		@Override
+		public String toString() {
+			return "MyButton [game=" + game + ", r=" + r + ", c=" + c + ", isHighlighted=" + isHighlighted + ", hasPiece="
+					+ hasPiece + "]";
+		}
 	}
 }
+
+
+
